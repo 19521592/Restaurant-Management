befea671193972ba3a4c11363a2fcb9edeb01fa2
DESKTOP-5ULTH59\ASUS <duchuong007@gmail.com>
2020-10-16 14:37:18 +0700

    Add file login

--- fTableManagement.Designer.cs
+++ fTableManagement.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Restaurant_Management
 {
-    partial class Form1
+    partial class fTableManagement
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "fTableManagement";
         }
 
         #endregion
     }
 }
-
--- fTableManagement.cs
+++ fTableManagement.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Restaurant_Management
 {
-    public partial class Form1 : Form
+    public partial class fTableManagement : Form
     {
-        public Form1()
+        public fTableManagement()
         {
             InitializeComponent();
         }
